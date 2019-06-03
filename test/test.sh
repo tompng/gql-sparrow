efafca45433c78134c1f8a2ea29229ffadff0c42
tompng <tomoyapenguin@gmail.com>
2019-06-03 20:19:34 +0900

    package.json&test.sh refactor

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 cd $(dirname $0)/..
 npm run build
-npm run generate
-cd test
-../node_modules/typescript/bin/tsc --strict --lib es2017 --noEmit typeTest.ts
+npm run generate test/schema.graphql test/generated/types.ts
+npm run ts-type-check test/typeTest.ts
