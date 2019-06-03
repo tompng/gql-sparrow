071d7b1032828face436e9120b913981f57e377d
tompng <tomoyapenguin@gmail.com>
2019-06-03 20:07:18 +0900

    fix test

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
-cd $(dirname $0)
-cd ..
+cd $(dirname $0)/..
 npm run build
+npm run generate
 cd test
-node ../dist/bin/typed-gqlbuilder-gen schema.graphql generated/types.ts
 ../node_modules/typescript/bin/tsc --strict --lib es2017 --noEmit typeTest.ts
--- test/typeTest.ts
+++ test/typeTest.ts
@@ -1,44 +1,52 @@
 import { DataTypeFromRootQuery } from './generated/types'
-const query = {
 
-}
-type IsEqual<T, U> = [T, U] extends [U, T] ? NullableEqual<T, U> : false
-type NullableEqual<T, U> = [null, null] extends [T, U]
-  ? NonNullEquals<Exclude<T, null>, Exclude<U, null>>
-  : null extends T ? false : null extends U ? false : NonNullEquals<T, U>
-type NonNullEquals<T, U> = [T, U] extends [(infer T0)[], (infer U0)[]] ? ObjectEquals<T0, U0> : T extends object ? ObjectEquals<T, U> : true
-type ObjectEquals<T, U> = {
-  [key in keyof(T) & keyof(U)]: IsEqual<T[key], U[key]>
-} extends { [key in keyof(T) & keyof(U)]: true } ? true : false
+type IsEqual<T, U> = [T, U] extends [U, T] ? true : false
 function isOK<T extends true>(): T | undefined { return }
+type IsStrictMode = string | null extends string ? false : true
+isOK<IsStrictMode>()
+
+isOK<IsEqual<
+  DataTypeFromRootQuery<{ field: 'feed'; query: { title: true } }>,
+  { title: string }[]
+>>()
+isOK<IsEqual<
+  DataTypeFromRootQuery<{ field: 'feed'; query: 'title' }>,
+  { title: string }[]
+>>()
+isOK<IsEqual<
+  DataTypeFromRootQuery<{ field: 'feed'; query: ['id', 'title'] }>,
+  { id: number; title: string }[]
+>>()
 
 isOK<IsEqual<
   DataTypeFromRootQuery<{
-    field: 'feed',
+    field: 'feed'
     query: {
-      ID: { field: 'id' },
-      author: ['id', 'name', 'email'],
+      ID: { field: 'id' }
+      author: ['id', 'name', 'email']
       title: true,
       content: true
       location: true
-      foobar: { params: { id: 1 }, field: 'foobar' } // TODO: fix
+      foobar: { params: { id: 1 } }
     }
   }>,
   {
     ID: number
-    author: { id: number; name: string; email: string }
+    author: { id: number; name: string | null; email: string }
     title: string
     content: string | null
-    location: { lon: number; lat: number }
+    location: { lon: number; lat: number } | null
     foobar: string
   }[]
 >>()
 
 isOK<IsEqual<
-  DataTypeFromRootQuery<{ field: 'post', params: { id: 1 }, query: {
+  DataTypeFromRootQuery<{ field: 'post'; params: { id: 1 }; query: {
     id: { field: 'title' }
     id2: { field: 'id' }
     title: { field: 'id' }
+    foobar: { field: 'id' }
+    author: { field: 'foobar'; params: { id: 1 } }
   } }>,
-  { id: string, title: number, id2: number }
+  { id: string; title: number; id2: number; foobar: number; author: string }
 >>()
